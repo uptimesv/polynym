181bec1e99cbba576f7c4c3a6f21208e84fc991d
deanmlittle <10921578+deanmlittle@users.noreply.github.com>
2019-07-30 03:45:03 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+const axios = require('axios'),
+bsv = require('bsv');
+
+function resolveType(id){
+    const types = [{
+        name: 'HandCash',
+        regex: new RegExp(/^[$][\a-z0-9\-_.]{4,50}$/)
+    },
+    {
+        name: 'P2PKH',
+        regex: new RegExp(/^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$/)
+    },
+    {
+        name: 'RelayX',
+        regex: new RegExp(/^[1][\a-z0-9]*$/)
+    },
+    {
+        name: 'PayMail',
+        regex: new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)
+    }];
+    for(let type of types) {
+        if(id.match(type.regex)){
+            return type.name;
+        }
+    };
+    return 'invalid';
+}
+
+//RelayX
+async function callRelayX(id) {
+    return axios.get('https://www.relayx.io/api/receivingAddress/' + id);
+}
+
+//HandCash
+async function callHandCash(id) {
+    id = id.replace('$','');
+    return axios.get('https://api.handcash.io/api/receivingAddress/' + id);
+}
+
+//PayMail
+async function callPayMail(id) {
+    let name = id.split('@');
+    let url = name.pop();
+    const paymail = await axios.get('http://' + url + '/.well-known/bsvalias');
+    let publicKeyURI = paymail.data.capabilities.pki.replace('{domain.tld}',url).replace('{alias}', name);
+    const pub = await axios.get(publicKeyURI);
+    const publicKey = bsv.PublicKey.fromHex(pub.data.pubkey);
+    const address = bsv.Address.fromPublicKey(publicKey);
+    return address.toString();
+}
+
+module.exports = async function polynym(id){
+    return new Promise(async(resolve, reject) => {
+        const type = resolveType(id);
+        if(type=='P2PKH'){
+            resolve({address: id});
+        } else if(type=='HandCash'){
+            try { 
+                let x = await callHandCash(id);
+                resolve({address: x.data.receivingAddress});
+            } catch(e){
+                reject({ error: '$handle not found' });
+            }
+        } else if(type=='RelayX'){
+            try { 
+                let x = await callRelayX(id);
+                if(x.data.receivingAddress){
+                    resolve({address: x.data.receivingAddress});
+                } else {
+                    reject({ error: '1handle not found' });
+                }
+            } catch(e){
+                reject(e)
+            }
+        } else if(type=='PayMail'){
+            try {
+                let x = await callPayMail(id);
+                resolve({address: x});
+            } catch(e){
+                reject({ error: 'PayMail not found' });
+            }
+        } else {
+            reject({ error: 'Unable to resolve to address' });
+        }    
+    });
+};
